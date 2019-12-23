200484e13760f6f1b93b08268fdfee42c8811b7a
Marijn <marijnvanwezel@gmail.com>
2019-12-23 11:46:03 +0100

    Update 3.0

--- src/classes/ComplexArrayAddValue.class.php
+++ src/classes/ComplexArrayAddValue.class.php
@@ -35,7 +35,8 @@ class ComplexArrayAddValue extends ResultPrinter {
         return [
             'caaddvalue',
             'caadd',
-            'caaddv'
+            'caaddv',
+            'caset'
         ];
     }
 
